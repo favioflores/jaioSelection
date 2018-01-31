263a4d638755541ebcf6467de299b39c9931d3e3
Flores Sanchez <Flores Sanchez@Laptop-Flores>
2018-01-30 21:30:49 -0500

    Agregar color Agregar pantalla de acceso denegado

--- JaioSelection/src/main/java/jaio/selection/filter/AuthFilter.java
+++ JaioSelection/src/main/java/jaio/selection/filter/AuthFilter.java
@@ -14,82 +14,80 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
- 
-@WebFilter(filterName = "AuthFilter", urlPatterns = {"*.xhtml"})
+
+//@WebFilter(filterName = "AuthFilter", urlPatterns = {"*.xhtml"})
 public class AuthFilter implements Filter {
-    
+
     private static Log log = LogFactory.getLog(AuthFilter.class);
-     
+
     public AuthFilter() {
     }
- 
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-         
+
     }
- 
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-         try {
- 
-            // check whether session variable is set
+        try {
+
             HttpServletRequest req = (HttpServletRequest) request;
             HttpServletResponse res = (HttpServletResponse) response;
             HttpSession ses = req.getSession(false);
-            
-            /* EVADIR OBJETOS INNECESARIOS */
-            
-            if(validaUri(req)){
-                
+
+            if (validaUri(req)) {
+
                 String reqURI = req.getRequestURI();
-                if ( reqURI.indexOf("/ui/iniciar.jsf") >= 0){
+                if (reqURI.indexOf("/login.jsf") >= 0) {
                     chain.doFilter(request, response);
                     log.debug("Sin validacion en login");
-                }else if (ses != null && 
-                          ses.getAttribute("usuarioInfo") != null || 
-                          reqURI.indexOf("/public/") >= 0 || 
-                          reqURI.contains("javax.faces.resource") ){
+                } else if (ses != null
+                        && ses.getAttribute(Constantes.SESSION_USUARIO) != null
+                        || reqURI.indexOf("/public/") >= 0
+                        || reqURI.contains("javax.faces.resource")) {
                     log.debug("Session valida");
                     chain.doFilter(request, response);
-                }else{   // user didn't log in but asking for a page that is not allowed so take user to login page
+                } else {
                     log.debug("Session invalida");
-                    res.sendRedirect(req.getContextPath() + "/ui/sesionExpirada.jsf");  // Anonymous user. Redirect to login page
+                    res.sendRedirect(req.getContextPath() + "/expiro.jsf");
                 }
-                
-            }else{
+
+            } else {
                 chain.doFilter(request, response);
             }
-            
-      }
-     catch(Throwable t) {
-         log.error(t);
-     }
+
+        } catch (Throwable t) {
+            log.error(t);
+        }
     } //doFilter
- 
+
     @Override
     public void destroy() {
-         
+
     }
-    
-    public boolean validaUri(HttpServletRequest req){
- 
+
+    public boolean validaUri(HttpServletRequest req) {
+
         String strUri = req.getServletPath();
-        
-        if(strUri.endsWith(Constantes.STR_CSS) ||
-           strUri.endsWith(Constantes.STR_GIF) ||
-           strUri.endsWith(Constantes.STR_PNG) ||
-           strUri.endsWith(Constantes.STR_JPG) ||
-           strUri.endsWith(Constantes.STR_HTM) ||
-           strUri.endsWith(Constantes.STR_JS)  ||
-           strUri.endsWith(Constantes.STR_SWF) ||
-           strUri.indexOf("/ui/iniciar") >= 0 ||
-           strUri.indexOf("/ui/clave") >= 0 ||
-           strUri.indexOf("/ui/sesionExpirada") >= 0){
-            
+
+        if (strUri.endsWith(Constantes.STR_CSS)
+                || strUri.endsWith(Constantes.STR_GIF)
+                || strUri.endsWith(Constantes.STR_PNG)
+                || strUri.endsWith(Constantes.STR_JPG)
+                || strUri.endsWith(Constantes.STR_HTM)
+                || strUri.endsWith(Constantes.STR_JS)
+                || strUri.endsWith(Constantes.STR_SWF)
+                || strUri.indexOf("/login") >= 0
+                || strUri.indexOf("/clave") >= 0
+                || strUri.indexOf("/error") >= 0
+                || strUri.indexOf("/404") >= 0
+                || strUri.indexOf("/access") >= 0) {
+
             return false;
-            
+
         }
-        
+
         return true;
     }
 }
